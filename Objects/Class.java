45434f72796ffcfd2c8134881a7929ed0c220fee
VismayaSKumar <vismayaskumar10@gmail.com>
2024-01-19 13:45:51 +0530

    edits

--- Objects/Class.java
+++ Objects/Class.java
@@ -1,9 +1,21 @@
 public class Class {
-    static int a=10; 
+
+    static int var=10; 
     int ab=19;
+
+    static void function(){
+        System.out.println("static function");
+    }
+
+    void normal()
+    {
+        System.out.println("normal function");
+    }
     public static void main(String[] args) {
-        System.out.println(a); // a can be accessed directly, coz of static keyword
+        System.out.println(var); //  can be accessed directly, coz of static keyword
+        function();
         Class obj= new Class();
-        System.out.println(obj.ab); //ab can be accessed only when object is created
+        System.out.println(obj.ab); // can be accessed only when object is created
+        obj.normal();
     }
 }
